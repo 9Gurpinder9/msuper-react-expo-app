9999d5ab0b7a78375386001a37992028e6d92bed
9Gurpinder9 <9Gurpinder9@users.noreply.github.com>
2026-02-01 09:18:43 +0530

    Add ESLint v9 flat config for frontend

---
+++ frontend/eslint.config.js
@@ -0,0 +1,21 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+
+// Bridge existing .eslintrc.js config to ESLint v9 flat config
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+module.exports = [
+  ...compat.config({
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "react", "react-hooks"],
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "prettier"
+    ],
+    parserOptions: { ecmaFeatures: { jsx: true } },
+    settings: { react: { version: "detect" } }
+  })
+];
